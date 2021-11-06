13cbe76c41c7810e8d6b005d52e2ba7e8d35a0dc
Muhammad Afaq Shuaib <afaq.shoaib09@gmail.com>
2021-11-07 00:48:18 +0500

    horizontal view updated and button functionality added

--- app/src/main/java/com/example/intentpractice/MainActivity2.java
+++ app/src/main/java/com/example/intentpractice/MainActivity2.java
@@ -1,5 +1,7 @@
 package com.example.intentpractice;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +14,13 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
     Button instaBtn;
     Button whatsappBtn;
     Button linkedinBtn;
+
+    public void openWebPage(String url)
+    {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +32,7 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
         instaBtn = findViewById(R.id.insta_btn);
         instaBtn.setOnClickListener(this);
 
-        whatsappBtn = findViewById(R.id.insta_btn);
+        whatsappBtn = findViewById(R.id.whatsapp_btn);
         whatsappBtn.setOnClickListener(this);
 
         linkedinBtn = findViewById(R.id.linked_btn);
@@ -33,6 +42,25 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.fb_btn:
+                openWebPage("https://www.facebook.com/afaq.shoaib.9/");
+                break;
+            case R.id.insta_btn:
+                openWebPage("https://www.instagram.com/afaq_shoaib/");
 
+                break;
+            case R.id.linked_btn:
+                openWebPage("https://www.linkedin.com/in/muhammad-afaq-shuaib/");
+                break;
+
+            case R.id.whatsapp_btn:
+                openWebPage("https://api.whatsapp.com/send?phone=923245227602");
+                break;
+
+            case R.id.back_btn:
+                System.out.println("hello");
+                this.finish();
+        }
     }
 }
